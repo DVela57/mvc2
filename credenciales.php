dd39ad6bd522065bd9c0c0bfd888411a35df07e7
Diego Vela <diegotron87@gmail.com>
2022-11-07 16:30:55 +0000

    mvc00

--- credenciales.php
+++
@@ -1,4 +0,0 @@
-<?php
-const dsn = "mysql:dbname=demo;host=db";
-  const usuario = "dbuser";
-  const clave = "secret";
---
+++ product.php
@@ -0,0 +1,24 @@
+<?php
+    //fochero que simula modelo con datos
+    class Product {
+        const PRODUCTS = [
+            array(1,'Cortacesped'), 
+            array(2,'Pizarra'), 
+            array(3,'Billar'), 
+            array(4,'Dardos')
+        ];
+
+        function __construct() /*const vacio*/ {}
+
+        //devuelve todos los productos
+        public static function all()
+        {
+            return Product::PRODUCTS;
+        }
+
+        //devolber un producto
+        public function find($id)
+        {
+            return Product::PRODUCTS[$id -1];
+        }
+    }
