1f72b713f4fb2cc4b063a604c950eb8d0f52bb41
Diego Vela <diegotron87@gmail.com>
2022-11-23 15:45:31 +0000

    añadir composer

--- start.php
+++ start.php
@@ -1,4 +1,8 @@
 <?php
+    use \Core\App;
     //print "<h2>Contenido Privado</h2>";
-    require_once "core/App.php";
-    $app = new \Core\App();
+    //require_once "core/App.php";
+
+    //autoload de composer
+    require 'vendor/autoload.php';
+    $app = new App();
